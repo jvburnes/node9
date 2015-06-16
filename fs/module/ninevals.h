6cd39522b1f1b4b0a89501c5cd9ab67a9bce1387
Jim Burnes <jvburnes@gmail.com>
2015-06-16 14:20:45 -0600

    LuaJIT FFI fix and revert

--- fs/module/ninevals.h
+++ fs/module/ninevals.h
@@ -1,13 +1,8 @@
 /* plan9/inferno-specific value types */
-#if defined(__linux__)
-  typedef unsigned long long int uvlong;
-  typedef long long int vlong;
-  typedef vlong u64int;
-#else
-  typedef  int64_t vlong;
-  typedef	uint64_t uvlong;
-#endif
-  typedef unsigned long ulong;
-  typedef unsigned short ushort;
-  typedef unsigned int uint;
-  typedef unsigned char uchar;
+typedef  int64_t vlong;
+typedef	uint64_t uvlong;
+typedef unsigned long ulong;
+typedef unsigned short ushort;
+typedef unsigned int uint;
+typedef unsigned char uchar;
+
